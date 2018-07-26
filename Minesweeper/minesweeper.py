abe67d73ae2d57e55b4e31150e6fccc87df4d41d
Huang <if994102@canada01.ifdsnet.int>
2018-07-26 14:00:00 -0400

    making the first move is not mine, and double click functionality activited

--- Minesweeper/minesweeper.py
+++ Minesweeper/minesweeper.py
@@ -5,113 +5,183 @@ import math as math
 
 
 class Minesweeper:
-    def __init__(self, master):
+    size_ops = {
+        "Beginner": (9, 9),
+        "Intermediate": (16, 16),
+        "Expert": (16, 30)
+    }
+
+    mines_ops = {
+        "Beginner": 10,
+        "Intermediate": 40,
+        "Expert": 99
+    }
+
+    colors_ops = {
+        1: "blue",
+        2: "green",
+        3: "red",
+        4: "purple",
+        5: "maroon",
+        6: "turquoise",
+        7: "black",
+        8: "gray"
+    }
+
+    def __init__(self, master, title="Minesweeper"):
         self.master = master
+        self.master.title(title)
+
+        self.__set_size()
         self.grid = []
         self.board = []
         self.status = []
-        self.finished = False
-        self.win = False
-        self.start_time = None
-        self.flag = tk.PhotoImage(file="flag.png").subsample(3)
-        self.mine = tk.PhotoImage(file="mine.png").subsample(8)
-        master.title("Minesweeper")
 
-        OPTIONS = [
-            "Beginner",
-            "Intermediate",
-            "Expert"
-        ]
+        self.flag = tk.PhotoImage(file="flag.png")
+        self.mine = tk.PhotoImage(file="mine.png")
+        self.dead_mine = tk.PhotoImage(file="dead_mine.png")
+        self.wrong_mine = tk.PhotoImage(file="wrong_mine.png")
+        self.smile = tk.PhotoImage(file="smile.png")
+        self.over = tk.PhotoImage(file="over.png")
+        self.cool = tk.PhotoImage(file="cool.png")
 
-        global var
-        var = tk.StringVar(master)
-        var.set(OPTIONS[0])
-
-        game = tk.OptionMenu(master, var, *OPTIONS)
-        game.grid(row=0, column=0, columnspan=5, sticky=tk.W)
-
-        button = tk.Button(master, text="OK", command=self.__set_size)
-        button.grid(row=0, column=5, columnspan=2, sticky=tk.W)
-
-        self.__set_size()
-
-    def __set_size(self):
-        OPTIONS = {
-            "Beginner": [9, 9],
-            "Intermediate": [16, 16],
-            "Expert": [16, 30]
-        }
-        self.n, self.m = OPTIONS[var.get()][0], OPTIONS[var.get()][1]
+        self.__set_menu_bar()
+        self.__set_counter()
         self.__set_grid()
-        self.__set_mines()
-        self.__start_game()
-        self.__set_timer()
 
-    def __start_game(self):
+    def __set_menu_bar(self):
+        """ Set the menu toolbar for change size and score board. """
+        self.menu = tk.Menu(self.master)
+        self.master.config(menu=self.menu)
+
+        self.submenu = tk.Menu(self.menu)
+        self.menu.add_cascade(label="Game", menu=self.submenu)
+        self.submenu.add_command(label="Beginner", command=lambda: self.__update_board("Beginner"))
+        self.submenu.add_command(label="Intermediate", command=lambda: self.__update_board("Intermediate"))
+        self.submenu.add_command(label="Expert", command=lambda: self.__update_board("Expert"))
+
+    def __set_counter(self):
+        """ Set up mines counter, smile face, and timer."""
+        # set up mines counter
+        self.mines_left_label = tk.Label()
+        self.mines_left_label.grid(row=0, columnspan=self.m, sticky=tk.W + tk.N + tk.S)
+        self.__update_mines_left()
+
+        # set up smile face
+        self.smile_face = tk.Button(image=self.smile)
+        self.smile_face.grid(row=0, columnspan=self.m, sticky=tk.N + tk.S)
+        self.smile_face.bind("<Button-1>", lambda e: self.__update_board(self.level))
+
+        # set up timer
+        self.timer = tk.Label(text=0)
+        self.timer.grid(row=0, columnspan=self.m, sticky=tk.E + tk.N + tk.S)
+
+    def __set_size(self, level="Beginner"):
+        self.level = level
+        self.n, self.m = self.size_ops[self.level]
+        self.total_mines = self.mines_ops[self.level]
+        self.mines_left = self.total_mines
         self.finished = False
-        self.win = False
+        self.mine_generated = False
         self.start_time = None
+        self.win = False
 
     def __set_grid(self):
-        for block in self.grid:
-            block.grid_forget()
-        self.board, self.grid, self.status = [], [], []
+        """ clean the old board and create new board. """
+        self.__clean_board()
         for i in range(self.n):
             for j in range(self.m):
                 label = tk.Label(self.master, bg="gray", borderwidth=2, relief="groove", height=1, width=2)
-                label.grid(row=i + 2, column=j, sticky=tk.W + tk.E + tk.N + tk.S)
+                label.grid(row=i + 1, column=j, sticky=tk.W + tk.E + tk.N + tk.S)
                 label.bind("<Button-1>", self.__left_click)
                 label.bind("<Button-3>", self.__right_click)
+                label.bind("<Double-Button-1>", self.__double_click)
                 self.grid.append(label)
                 self.board.append(0)
                 self.status.append(False)
 
-    def __left_click(self, e):
+    def __generate_mines(self, ignore):
+        self.mines = random.sample([x for x in range(self.n * self.m) if x != ignore], self.total_mines)
+        self.flags = []
+        for mine in self.mines:
+            self.__set_mine(mine)
+        self.mine_generated = True
+
+    def __set_mine(self, mine):
+        self.board[mine] = 9
+        for block in self.__find_neighbor(mine):
+            self.board[block] += 1
+
+    def __update_board(self, level):
+        self.__set_size(level)
+        self.mines_left_label.grid_forget()
+        self.smile_face.grid_forget()
+        self.timer.grid_forget()
+        self.__set_counter()
+        self.__set_grid()
+
+    def __update_mines_left(self):
+        self.mines_left_label.config(text=max(self.mines_left, 0))
+
+    def __update_timer(self):
+        if self.start_time is not None and not self.finished:
+            now = time.time() - self.start_time
+            self.timer.config(text=math.floor(now))
+            self.master.after(1000, self.__update_timer)
+
+    def __left_click(self, e, block=None):
         if self.finished:
             return
 
         if self.start_time is None:
             self.start_time = time.time()
-            self.__update_clock()
+            self.__update_timer()
 
-        block = self.grid.index(e.widget)
+        block = block if block else self.grid.index(e.widget)
+
+        if not self.mine_generated:
+            self.__generate_mines(block)
+
         if self.status[block] == 'f':
             return
         elif self.board[block] > 8:
             self.finished = True
+            self.grid[block].config(image=self.dead_mine, anchor=tk.CENTER)
+            self.mines.remove(block)
             for mine in self.mines:
-                self.grid[mine].config(image=self.mine, anchor=tk.CENTER)
+                if mine in self.flags:
+                    self.grid[mine].config(image=self.flag, anchor=tk.CENTER)
+                    self.flags.remove(mine)
+                else:
+                    self.grid[mine].config(image=self.mine, anchor=tk.CENTER)
+            for mine in self.flags:
+                self.grid[mine].config(image=self.wrong_mine, anchor=tk.CENTER)
         elif not self.status[block]:
             self.__open_area(block)
             if self.finished:
                 for mine in self.mines:
                     self.grid[mine].config(image=self.flag, anchor=tk.CENTER)
                 self.num_mines_left = 0
-                self.__update_num_mines_left()
+                self.__update_mines_left()
+
+        if self.finished:
+            self.smile_face.config(image=self.cool if self.win else self.over)
 
     def __open_area(self, block):
         if block < 0 or block >= self.n * self.m or self.status[block]:
             return
         elif self.board[block] > 0:
-            self.grid[block].config(text=self.board[block], bg="darkgray")
+            self.grid[block].config(text=self.board[block],
+                                    bg="darkgray", fg=self.colors_ops[self.board[block]])
             self.status[block] = True
-            if self.status.count(True) == self.n * self.m - self.num_mines:
+            if self.status.count(True) == self.n * self.m - self.total_mines:
                 self.finished = True
                 self.win = True
         else:
             self.grid[block].config(bg="darkgray")
             self.status[block] = True
-            self.__open_horizontal(block)
-            if block % self.m != 0:
-                self.__open_area(block - 1)
-                self.__open_horizontal(block - 1)
-            if block % self.m != self.m - 1:
-                self.__open_area(block + 1)
-                self.__open_horizontal(block + 1)
-
-    def __open_horizontal(self, block):
-        self.__open_area(block - self.m)
-        self.__open_area(block + self.m)
+            for block in self.__find_neighbor(block):
+                self.__open_area(block)
 
     def __right_click(self, e):
         if self.finished:
@@ -119,74 +189,55 @@ class Minesweeper:
 
         if self.start_time is None:
             self.start_time = time.time()
-            self.__update_clock()
+            self.__update_timer()
 
         block = self.grid.index(e.widget)
+
         if self.status[block] == 'f':
-            e.widget.config(image="")
+            e.widget.config(image='')
+            self.flags.remove(block)
             self.status[block] = False
-            self.num_mines_left += 1
-            self.__update_num_mines_left()
+            self.mines_left += 1
         elif not self.status[block]:
             e.widget.config(image=self.flag)
+            self.flags.append(block)
             self.status[block] = 'f'
-            self.num_mines_left -= 1
-            self.__update_num_mines_left()
+            self.mines_left -= 1
+        self.__update_mines_left()
 
-    def __set_mines(self):
-        OPTIONS = {
-            "Beginner": 10,
-            "Intermediate": 40,
-            "Expert": 99
-        }
-        self.num_mines = OPTIONS[var.get()]
+    def __double_click(self, e):
+        block = self.grid.index(e.widget)
+        if self.status[block] is True and self.board[block] > 0:
+            neighbor = {key: self.status[key] for key in self.__find_neighbor(block)}
+            if list(neighbor.values()).count('f') == self.board[block]:
+                for key in neighbor.keys():
+                    if not neighbor[key]:
+                        self.__left_click(e, key)
 
-        self.num_mines_left = self.num_mines
-        self.num_mines_left_label = tk.Label()
-        self.num_mines_left_label.grid(row=1, columnspan=self.m, sticky=tk.W)
-        self.__update_num_mines_left()
+    def __find_neighbor(self, block):
+        neighbor = [x for x in self.__check_horizontal(block)]
+        if block % self.m != 0:
+            neighbor.append(block - 1)
+            neighbor += self.__check_horizontal(block - 1)
+        if block % self.m != self.m - 1:
+            neighbor.append(block + 1)
+            neighbor += self.__check_horizontal(block + 1)
+        return neighbor
 
-        self.mines = random.sample(range(self.n * self.m), self.num_mines)
-        for mine in self.mines:
-            self.__set_mine(mine)
-        self.__print_board()
+    def __check_horizontal(self, block):
+        res = []
+        if block - self.m >= 0:
+            res.append(block - self.m)
+        if block + self.m < self.n * self.m:
+            res.append(block + self.m)
+        return res
 
-    def __set_mine(self, mine):
-        self.board[mine] = 9
-        self.__check_horizontal(mine)
-        if mine % self.m != 0:
-            self.board[mine - 1] += 1
-            self.__check_horizontal(mine - 1)
-        if mine % self.m != self.m - 1:
-            self.board[mine + 1] += 1
-            self.__check_horizontal(mine + 1)
-
-    def __check_horizontal(self, mine):
-        if mine - self.m >= 0:
-            self.board[mine - self.m] += 1
-        if mine + self.m < self.n * self.m:
-            self.board[mine + self.m] += 1
-
-    def __set_timer(self):
-        self.timer = tk.Label(text="0")
-        self.timer.grid(row=1, columnspan=self.m, sticky=tk.E)
-
-    def __update_clock(self):
-        if self.start_time is not None and not self.finished:
-            now = time.time() - self.start_time
-            self.timer.config(text=math.floor(now))
-            self.master.after(1000, self.__update_clock)
-
-    def __update_num_mines_left(self):
-        self.num_mines_left_label.config(text=max(self.num_mines_left, 0))
-
-    def __print_board(self):
-        print("#" * self.m + " board " + "#" * self.m)
-        for i in range(self.n * self.m):
-            if i % self.m != self.m - 1:
-                print("X" if self.board[i] > 8 else self.board[i], end="\t")
-            else:
-                print("X" if self.board[i] > 8 else self.board[i])
+    def __clean_board(self):
+        for button in self.grid:
+            button.grid_forget()
+        self.grid = []
+        self.board = []
+        self.status = []
 
 
 def main():
