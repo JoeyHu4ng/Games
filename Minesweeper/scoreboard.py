07678d228e470fd5cbcadad94a4317f28b54f8a4
Huang <if994102@canada01.ifdsnet.int>
2018-07-27 12:11:00 -0400

    expand rank grid one more width to show whole rank for No.10

--- Minesweeper/scoreboard.py
+++ Minesweeper/scoreboard.py
@@ -3,7 +3,9 @@ import tkinter as tk
 
 class Scoreboard:
 
-    def __init__(self, scoreboard={key: [] for key in ["Beginner", "Intermediate", "Expert"]}):
+    def __init__(self, scoreboard=None):
+        if scoreboard is None:
+            scoreboard = {key: [] for key in ["Beginner", "Intermediate", "Expert"]}
         self.mater = tk.Tk()
         self.mater.title("Scoreboard")
         self.scoreboard = scoreboard
@@ -20,8 +22,8 @@ class Scoreboard:
     def __show_data(self, level="Beginner"):
         scores = self.scoreboard[level]
         for i in range(10):
-            rank = tk.Label(self.mater, height=1, width=3, text="No. %d" % (i + 1))
-            rank.grid(row=i, column=0, columnspan=3, sticky=tk.W + tk.E + tk.N + tk.S)
+            rank = tk.Label(self.mater, height=1, width=4, text="No. %d" % (i + 1))
+            rank.grid(row=i, column=0, columnspan=4, sticky=tk.W + tk.E + tk.N + tk.S)
 
             if i < len(scores):
                 grade, name = scores[i]
@@ -29,9 +31,9 @@ class Scoreboard:
                 grade, name = "-" * 6, "-" * 20
 
             name_label = tk.Label(self.mater, height=1, width=20, text=name)
-            name_label.grid(row=i, column=3, columnspan=20, sticky=tk.W + tk.E + tk.N + tk.S)
+            name_label.grid(row=i, column=4, columnspan=20, sticky=tk.W + tk.E + tk.N + tk.S)
             grade_label = tk.Label(self.mater, height=1, width=6, text=grade)
-            grade_label.grid(row=i, column=23, columnspan=6, sticky=tk.W + tk.E + tk.N + tk.S)
+            grade_label.grid(row=i, column=24, columnspan=6, sticky=tk.W + tk.E + tk.N + tk.S)
 
 
 if __name__ == "__main__":
